5b944d3a244484d546847195cd68638e5fdab04a
JULIA FIORENTINO <julia.fiorentino@senacsp.edu.br>
2023-10-10 22:23:53 -0300

    Primeiro Commit

---
+++ ProjetoTI18N/Calculadora.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTI18N
+{
+     class Calculadora
+    {
+        //Variaveis globais
+        private double num1;
+        private double num2;
+        //Método construtor -> Instância as variáveis na memória do computador
+        public Calculadora()
+
+        {
+            ConsularNum1 = 0;
+            ConsultarNum2 = 0;
+        }// Fim do método construtor
+
+        //Métodos Modificadores = GET e SET
+        Public double ConsultarNum1
+        {
+            get { return this.num1; }
+            set { this.num1 = value; }
+
+        }// Fim do método ConsultarNum1
+
+        //Métodos Modificadores = GET e SET
+        Public double ConsultarNum2
+        {
+            get { return this.num2; }
+            set { this.num2 = value; }
+
+        }// Fim do método ConsultarNum2
+
+        //Métodos operções
+        public double Somar()
+        {
+            return ConsultarNum1 + ConsultarNum2;
+        }//Fim Somar
+
+        public double Subtrair()
+        {
+            return ConsultarNum1 - ConsultarNum1;
+        }//Fim subtração
+
+        public double Multiplicar()
+        {
+            return ConsultarNum1 * ConsultarNum2;
+        }//Fim Multiplicar
+
+        public double Dividir ()
+        {
+            if (ConsultarNum2 <= 0)
+            {
+                return -1;
+
+            }
+            else
+            {
+
+                return ConsultarNum1 / ConsultarNum2;
+            }
+           
+
+        }
+
+    }// Fim Da Classe
+}// Fim do Projeto
---
+++ ProjetoTI18N/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTI18N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();//Leia - uso
+        }// Fim do método main 
+    }// Fim da classe Program 
+}// Fim  do Projeto
